051bc773e611a33665af20eecbbb554d0a1c2c9e
jmdelvalle <jeremy.delvalle@flatironschool.com>
2015-10-09 18:41:53 -0400

    Trying to mess around with create new post and show blog after

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,21 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @posts = @blog.posts
+    @blog = Blog.find(params[:id])
   end
+
+  def new
+    @blog = Blog.new
+    # @posts = @blog.posts
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+    redirect_to "/posts/new"
+  end
+end
+private
+
+def blog_params
+  params.require(:blog).permit(:name, :subdomain)
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def show
     @post = @blog.posts.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+    redirect_to "/blogs/show"
+  end
+
+  def create
+    @post = Post.create(post_params)
+  end
+end
+
+private
+
+def post_params
+  params.require(@post).permit(:title, :body)
 end

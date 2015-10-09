2e1c6098811af0e8b361ab1ac5169f33e8624d45
Omrika <omrikafolkes@gmail.com>
2015-10-09 17:27:19 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Blog.delete_all
+Blog.create(id: 1, name: "Omrika", subdomain: "omrikablog")
+Blog.create(id: 2, name: "Jeremy", subdomain: "jermblog")
+Blog.create(id: 3, name: "Gian", subdomain: "gianblog")
+Blog.create(id: 4, name: "Uzo", subdomain: "uzoblog")
+Blog.create(id: 5, name: "Ben", subdomain: "benblog")
+
+Post.delete_all
+Post.create(id: 1, blog_id: 1, title: "An Example of a Post", body: "This is a perfect example of a blog post.  Feel free to reference this example in your other applications.  Note that the author of this blog post does not accept responsibility for the contents of this blog post.")
+Post.create(id: 2, blog_id: 1, title: "Another Example of a Post", body: "This is yet another example of a blog post.  This one is less perfect than the first one.")
+Post.create(id: 3, blog_id: 2, title: "An Utterly Awesome Post", body: "This is a super awesome example of a really good blog post.  You should...like...totally copy this!")
+Post.create(id: 4, blog_id: 2, title: "Yet Another Utterly Post", body: "This is yet ANOTHER example of a super awesome blog post.  You should totally copy this one as well!")
+Post.create(id: 4, blog_id: 2, title: "Yet Another Utterly Post", body: "This is yet ANOTHER example of a super awesome blog post.  You should totally copy this one as well!")
+

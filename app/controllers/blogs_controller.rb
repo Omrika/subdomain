916aad54493b91912f33ca09d9f5bce37f7ee4db
jmdelvalle <jeremy.delvalle@flatironschool.com>
2015-10-10 13:13:56 -0400

    Created a simple subdomain generator. Need to add two links in the show view under blogs.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,4 +6,17 @@ class BlogsController < ApplicationController
   def show
     @posts = @blog.posts
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+  end
+end
+private
+
+def blog_params
+  params.require(:blog).permit(:name, :subdomain)
 end
